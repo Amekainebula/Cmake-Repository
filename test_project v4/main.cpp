bc2fc9b27173d4cefaef9610812bf5958306b82a
Amekainebula <hosizora_starry@qq.com>
2026-06-01 18:02:35 +0800

    update

--- test_project v4/main.cpp
+++ test_project v4/main.cpp
@@ -4,11 +4,14 @@
 int main()
 {
     std::cout << "Hello CMake in VS Code!" << std::endl;
+#ifdef DEBUG
+    std::cout << "Debug!\n";
+#endif
     int a, b;
     std::cin >> a >> b;
-    std::cout << add(a, b) << std::endl;
-    std::cout << subtract(a, b) << std::endl;
-    std::cout << multiply(a, b) << std::endl;
-    std::cout << divide(a, b) << std::endl;
+    std::cout << "a + b = " << add(a, b) << std::endl;
+    std::cout << "a - b = " << subtract(a, b) << std::endl;
+    std::cout << "a * b = " << multiply(a, b) << std::endl;
+    std::cout << "a / b = " << divide(a, b) << std::endl;
     return 0;
 }
